5e111b8e928e03716380fe1331d504f5373eae90
Charlyln <charleshenry.lenue@gmail.com>
2020-06-25 01:55:30 +0200

    add axios metod to register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from 'react-router-dom';
 import { Container, Form, FormGroup, Label, Input } from "reactstrap";
 import Squirrel from "../img/logo_squicker_fond.png";
 import style from "./Connexion.module.css";
+import Axios from "axios";
 
 export default function Connexion() {
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+
+  const postPatient = async () => {
+    try {
+      const res = await Axios.post("http://localhost:8000/patients", {
+        lastname,
+        firstname,
+      });
+      localStorage.setItem("uuid", res.data.uuid);
+      // if (window.localStorage.getItem('uuid')){
+      //   return <Redirect to="/" />
+      // }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Container>
       <img className={style.logo} src={Squirrel} alt="logo Squicker" />
@@ -12,17 +32,22 @@ export default function Connexion() {
         Bonjour, j'espère que vous allez bien aujourd'hui? Ne vous en faites
         pas, vous enregistrez ne prendra que quelques secondes.
       </p>
-      <Form>
+      <Form onSubmit={postPatient}>
         <FormGroup>
           <Label for="Nom">Nom</Label>
-          <Input type="text" name="name" id="Nom" placeholder="votre nom" />
+          <Input
+            type="text"
+            name="name"
+            placeholder="votre nom"
+            onChange={(e) => setLastname(e.target.value)}
+          />
         </FormGroup>
         <FormGroup>
           <Label for="Prenom">Prenom</Label>
           <Input
             type="text"
             name="name"
-            id="Prenom"
+            onChange={(e) => setFirstname(e.target.value)}
             placeholder="votre prenom"
           />
         </FormGroup>
