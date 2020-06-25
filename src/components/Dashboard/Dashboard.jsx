abbd3149ceca64d16d01c245d06f95cfd009e79a
Charlyln <charleshenry.lenue@gmail.com>
2020-06-25 12:19:29 +0200

    add heakthBook file

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -14,6 +14,7 @@ import Citations from "./Citations";
 import Zoom from "react-reveal/Zoom";
 import styles from "./dashboard.module.css";
 import DoctorData from "./DoctorData";
+import { Link } from "react-router-dom";
 
 export default function Dashboard(props) {
   const [modal, setModal] = useState(false);
@@ -30,7 +31,7 @@ export default function Dashboard(props) {
             md={{ size: "8", offset: 2 }}
             lg={{ size: "8", offset: 2 }}
           >
-            <div>
+            <Link to="./healthBook">
               <Card
                 onClick={toggle}
                 className="mt-3"
@@ -42,29 +43,7 @@ export default function Dashboard(props) {
                   borderRadius: "10px",
                 }}
               ></Card>
-
-              <Modal isOpen={modal} toggle={toggle} className={className}>
-                <ModalHeader toggle={toggle}>Carnet de Santé</ModalHeader>
-                <ModalBody>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="primary" onClick={toggle}>
-                    Valider
-                  </Button>{" "}
-                  <Button color="secondary" onClick={toggle}>
-                    Annulé
-                  </Button>
-                </ModalFooter>
-              </Modal>
-            </div>
+            </Link>
           </Col>
         </Row>
         <Row>
